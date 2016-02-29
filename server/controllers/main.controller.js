eeb0256fa2c45f656bff0fb837fac7d54446ac7e
arrC <naveen@arrc.in>
2016-02-29 19:33:21 +0530

    created get/post endpoint of e2e poc testing

--- server/controllers/main.controller.js
+++ server/controllers/main.controller.js
@@ -8,3 +8,13 @@ let chance = require('chance').Chance();
 exports.index = function(req, res, next) {
 	res.render('index', {clientScripts : clientScripts});
 };
+
+exports.list = function(req, res){
+	res.status(200).send({'data':['324','car', 'bike'], message: 'successful'});
+};
+
+exports.getUrl = function(req, res){
+	var b = req.body;
+	console.log(b);
+	res.status(200).send({'data':b, message: 'successful'});
+};
--- server/routes.js
+++ server/routes.js
@@ -4,13 +4,14 @@ module.exports = function(app){
 	var main = require('./controllers/main.controller.js');
 	var user = require('./controllers/user.controller.js');
 
-	// 'CORE' ----------------------------
+	// 'MAIN' ----------------------------
 	app.route('/').get(main.index);
+	app.route('/list').get(main.list);
+	app.route('/save').post(main.getUrl);
 
 	// 'USER'
 	app.route('/login').post(user.login);
 	app.route('/signup').post(user.signup);
 	app.route('/api/profile').get(user.profile);
-
 
 };
